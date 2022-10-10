204dda5800dca5146203ab1e2b469343793c1726
Jimy Marletta <84329916+jimymltta@users.noreply.github.com>
2022-10-10 15:56:37 +0200

    file and folder structure + navigation

---
+++ src/components/MovieInformations/MovieInformations.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MovieInformations = () => {
+  console.log('MovieInformations');
+  return (
+    <div>MovieInformations</div>
+  );
+};
+
+export default MovieInformations;
